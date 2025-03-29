1c003e9b8239d6f3e893ddc44f1c918128129663
Bianca <143421359+olarubianca6@users.noreply.github.com>
2025-03-29 16:12:58 +0200

    integration

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCIuq2VhC7xO9VzTNrcWEqg7KIU51O_mG4",
@@ -12,5 +12,5 @@ const firebaseConfig = {
   };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
 export default app;
